a1c372846b61dd4461fb98851dd4744b4e24806b
Michael Burri <michael.burri@syonix.ch>
2023-12-07 17:22:33 +0100

    Initial commit

---
+++ suffix.js
@@ -0,0 +1,24 @@
+async function getIcon(name) {
+    if (!(name in icons)) {
+        console.log(`Icon "${name}" not available`);
+        return '';
+    }
+
+    var svgDef = icons[name];
+    return {
+        path: svgDef[4],
+        viewBox: svgDef[0] + " " + svgDef[1] + " " + svgDef[2] + " " + svgDef[3]
+    }
+}
+
+async function getIconList() {
+    return Object.entries(icons).map(([icon]) => ({
+        name: icon
+    }));
+}
+
+window.customIconsets = window.customIconsets || {};
+window.customIconsets['feller'] = getIcon;
+
+window.customIcons = window.customIcons || {};
+window.customIcons['feller'] = { getIcon, getIconList };
